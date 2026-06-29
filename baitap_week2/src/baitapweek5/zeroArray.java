40964adbc2399ceabf49b1768a1166a0553f850d
thienan <thienan181203@gmail.com>
2026-06-29 14:37:07 +1000

    sửa

--- baitap_week2/src/baitapweek5/zeroArray.java
+++ baitap_week2/src/baitapweek5/zeroArray.java
@@ -3,27 +3,19 @@ package baitapweek5;
 public class zeroArray {
 
 	static void pushZerosToEnd(int[] arr) {
-        int n = arr.length;
-        int[] temp = new int[n];
-
-        // to keep track of the index in temp[]
-        int j = 0;
-
-        // Copy non-zero elements to temp[]
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != 0)
-                temp[j++] = arr[i];
+        int j =0;
+        
+        for (int i = 0; i < arr.length; i++) {
+        	if (arr[i] != 0) {
+        		int temp = arr[i];
+        		arr[i] = arr[j];
+        		arr[j] = temp;
+        		j++;
+        		
+        	}
+        	}
         }
-
-        // Fill remaining positions in temp[] with zeros
-        while (j < n)
-            temp[j++] = 0;
-
-        // Copy all the elements from temp[] to arr[]
-        for (int i = 0; i < n; i++)
-            arr[i] = temp[i];
-    }
-
+	
     public static void main(String[] args) {
         int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
         pushZerosToEnd(arr);
