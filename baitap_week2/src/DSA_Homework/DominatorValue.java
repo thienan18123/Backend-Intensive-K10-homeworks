10b5243207ac92ecc721ef8e6bb54af2eecd6d59
thienan <thienan181203@gmail.com>
2026-07-02 12:59:09 +1000

    update

--- baitap_week2/src/DSA_Homework/DominatorValue.java
+++ baitap_week2/src/DSA_Homework/DominatorValue.java
@@ -1,46 +1,64 @@
 package DSA_Homework;
+import java.util.HashMap;
 
 public class DominatorValue {
 	
+//	public int solution(int[] A) {
+//		
+//		//Boyer Moore
+//		int candidate = 0;
+//
+//		int  count = 0;
+//		
+//		for (int i = 0; i < A.length; i++) {
+//			if(count == 0) {
+//			candidate = A[i];
+//			count = 1;
+//		} else if (A[i] == candidate) {
+//			count++;
+//		} else {
+//			count--;
+//			}
+//		}
+//		int occurences = 0;
+//		int candidateIndex = -1;
+//		for (int i = 0; i < A.length; i++) {
+//			if (A[i] == candidate) {
+//				occurences++;
+//				candidateIndex = i;
+//				
+//			}
+//			
+//		}
+//		if (occurences > A.length /2) {
+//			return candidateIndex;
+//		}
+//		return -1;
+//		
+//	}
+	
+	//hashmap
 	public int solution(int[] A) {
+		if (A.length ==0) return -1;
 		
-		//Boyer Moore
-		int candidate = 0;
-
-		int  count = 0;
+		HashMap<Integer, Integer> counts = new HashMap<>();
 		
 		for (int i = 0; i < A.length; i++) {
-			if(count == 0) {
-			candidate = A[i];
-			count = 1;
-		} else if (A[i] == candidate) {
-			count++;
-		} else {
-			count--;
+			int val = A[i];
+			int c = counts.getOrDefault(val, 0) + 1;
+			counts.put(val, c);
+			if (c > A.length / 2) {
+				return i;
 			}
-		}
-		int occurences = 0;
-		int candidateIndex = -1;
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] == candidate) {
-				occurences++;
-				candidateIndex = i;
-				
-			}
-			
-		}
-		if (occurences > A.length /2) {
-			return candidateIndex;
 		}
 		return -1;
-		
 	}
 	
 	
 
 	public static void main(String[] args) {
 		
-		int[] A = {3,1,3,1,3,2,3};
+		int[] A = {3,3,3,3,3,3,1};
 		
 		DominatorValue dominator = new DominatorValue();
 		
