99f48922dff9f15a4acebf6c8e26d2cab3e0ce04
thienan <thienan181203@gmail.com>
2026-07-06 22:07:32 +1000

    sửa

--- baitap_week2/src/DSA_Homework/FrogJump.java
+++ baitap_week2/src/DSA_Homework/FrogJump.java
@@ -6,63 +6,52 @@ public class FrogJump {
 	
 	public static int solution(int[] A) {
 		
-	
-	//O(nlogn)- Breadth first search
+
 	
 	//fibonacci
 	int[] fib =  new int[26];
 	fib[0] = 1;
 	fib[1] = 2;
-	int fs = 2;
-	while (fib[fs - 1] < A.length) {
-		fib[fs] = fib[fs-1] + fib[fs-2];
-		fs++;
+	int fibNums = 2; //bao nhieu so fibonacci 
+	while (fib[fibNums - 1] < A.length) {
+		fib[fibNums] = fib[fibNums-1] + fib[fibNums-2];
+		fibNums++;
 	
 		
 	}
+	
+	//setup
 	boolean[] visited = new boolean[A.length];
 	Queue<int[]> queue = new ArrayDeque<>();
 	queue.add(new int[] {-1,0}); 
 	
-	
+	//logic
 	while (!queue.isEmpty()) {
-		int[] curr = queue.poll();
-		int pos = curr[0];
-		int jumps = curr[1];
+		int[] current = queue.poll();
+		int frogPos = current[0];
+		int jumps = current[1];
 		
-		for (int i = 0; i < fs; i++) {
-			int path = pos + fib[i];
+		for (int i = 0; i < fibNums; i++) {
+			int frogPath = frogPos + fib[i];
 			
-			if (path == A.length) {
+			if (frogPath == A.length) {
 				return jumps + 1;
 				
 				
 			}
-			if (path < A.length && A[path] == 1 && !visited[path]) { 
-				visited[path] = true;
-				queue.add(new int[] {path, jumps + 1});
+			if (frogPath < A.length && A[frogPath] == 1 && !visited[frogPath]) { 
+				visited[frogPath] = true;
+				queue.add(new int[] {frogPath, jumps + 1});
 			}
 		}
 		}
 	return -1;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-		
 
 	public static void main(String[] args) {
-//		int[] A = {0, 0, 1, 0, 1, 1, 0, 0}; //3
+
 		int[] A = {1,1,0,0,1,0,0,1,0,0};
+		
 		
     
 		
